ab6345d46c829080c4ccf65563f80f278a373124
Chatchai Saratakij <CSaratakij@gmail.com>
2018-08-20 10:44:02 +0700

    Make program to calculate a sum of two number

--- CGM208 - Lesson 1/MainForm.Designer.cs
+++ CGM208 - Lesson 1/MainForm.Designer.cs
@@ -29,11 +29,15 @@
         private void InitializeComponent()
         {
             this.btnHelloWorld = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtNumA = new System.Windows.Forms.TextBox();
+            this.txtNumB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnHelloWorld
             // 
-            this.btnHelloWorld.Location = new System.Drawing.Point(101, 52);
+            this.btnHelloWorld.Location = new System.Drawing.Point(7, 53);
             this.btnHelloWorld.Name = "btnHelloWorld";
             this.btnHelloWorld.Size = new System.Drawing.Size(105, 41);
             this.btnHelloWorld.TabIndex = 0;
@@ -41,21 +45,65 @@
             this.btnHelloWorld.UseVisualStyleBackColor = true;
             this.btnHelloWorld.Click += new System.EventHandler(this.btnHelloWorld_Click);
             // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(129, 53);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(105, 41);
+            this.btnCalculate.TabIndex = 1;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtNumA
+            // 
+            this.txtNumA.Location = new System.Drawing.Point(7, 12);
+            this.txtNumA.Name = "txtNumA";
+            this.txtNumA.Size = new System.Drawing.Size(100, 20);
+            this.txtNumA.TabIndex = 2;
+            this.txtNumA.Text = "0";
+            // 
+            // txtNumB
+            // 
+            this.txtNumB.Location = new System.Drawing.Point(134, 12);
+            this.txtNumB.Name = "txtNumB";
+            this.txtNumB.Size = new System.Drawing.Size(100, 20);
+            this.txtNumB.TabIndex = 3;
+            this.txtNumB.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(113, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "+";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(324, 142);
+            this.ClientSize = new System.Drawing.Size(257, 119);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNumB);
+            this.Controls.Add(this.txtNumA);
+            this.Controls.Add(this.btnCalculate);
             this.Controls.Add(this.btnHelloWorld);
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnHelloWorld;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtNumA;
+        private System.Windows.Forms.TextBox txtNumB;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- CGM208 - Lesson 1/MainForm.cs
+++ CGM208 - Lesson 1/MainForm.cs
@@ -21,5 +21,37 @@ namespace CGM208___Lesson_1
         {
             MessageBox.Show("Hello World");
         }
+
+        void btnCalculate_Click(object sender, EventArgs e)
+        {
+            var textResultFormat = "Result : {0}";
+
+            var numA = 0;
+            var numB = 0;
+
+            var isCanParseA = int.TryParse(txtNumA.Text, out numA);
+            var isCanParseB = int.TryParse(txtNumA.Text, out numB);
+
+            if (isCanParseA && isCanParseB) {
+                var result = Sum(numA, numB);
+                var resultText = string.Format(textResultFormat, result);
+                MessageBox.Show(resultText);
+            }
+            else {
+                MessageBox.Show("Please enter only number..");
+                ClearTextBox();
+            }
+        }
+
+        int Sum(int numA, int numB)
+        {
+            return numA + numB;
+        }
+
+        void ClearTextBox()
+        {
+            txtNumA.Text = "0";
+            txtNumB.Text = "0";
+        }
     }
 }
